059c1d6bf09ec872bd5147fa00a38317e4ad4fb6
sam <team2mdp@gmail.com>
2022-12-30 14:25:36 -0600

    refactored editbutton to updatebutton

--- ContactApp/Components/ContactDetailViewController/ContactDetailViewController.swift
+++ ContactApp/Components/ContactDetailViewController/ContactDetailViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ContactDetailViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     private var viewModel: ContactDetailViewControllerViewModelProtocol!
-    private var editButton: UIBarButtonItem!
+    private var updateButton: UIBarButtonItem!
     private var headerView: HeaderView!
     
     init(viewModel: ContactDetailViewControllerViewModelProtocol) {
@@ -48,10 +48,10 @@ class ContactDetailViewController: UIViewController {
         
         tableView.tableHeaderView = headerView
         
-        editButton = editButtonItem
-        editButton.tintColor = .systemBlue
-        editButton.action = #selector(edit)
-        navigationItem.rightBarButtonItem = editButton
+        updateButton = editButtonItem
+        updateButton.tintColor = .systemBlue
+        updateButton.action = #selector(edit)
+        navigationItem.rightBarButtonItem = updateButton
     }
 }
 
